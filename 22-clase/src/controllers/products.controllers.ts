113dc0c7e9be9dcef7cebda059c2db473156b7a8
marcostrejo2644 <trejomarcosf@live.com>
2021-09-23 21:16:01 -0300

    Entregable clase 22

---
+++ 22-clase/src/controllers/products.controllers.ts
@@ -0,0 +1,100 @@
+import { Request, Response } from 'express';
+import faker from 'faker';
+import Product from '../models/product.mongo';
+
+export async function getProductsFaker(req: Request, res: Response) {
+  const { cant } = req.query;
+  const products = [];
+  const quantity = Number(cant) || 0;
+
+  const fakerProducts = {
+    title: faker.commerce.productName(),
+    price: Number(faker.commerce.price()),
+    thumbnail: faker.image.technics(),
+  };
+
+  if (typeof quantity === 'number') {
+    if (quantity === 0) {
+      return res.status(404).json({ message: 'There`s no products' });
+    } else {
+      for (let i = 0; i < quantity; i++) {
+        products.push(fakerProducts);
+      }
+      return res.json({ products });
+    }
+  }
+
+  for (let i = 0; i < 10; i++) {
+    products.push(fakerProducts);
+  }
+  return res.json({ products });
+}
+
+export async function listAllProducts(req: Request, res: Response) {
+  try {
+    const products: any = await Product.find({});
+    products > 0
+      ? res.status(200).json(products)
+      : res.status(404).json({ messages: "There's no products" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something goes wrong' });
+  }
+}
+
+export async function addNewProduct(req: Request, res: Response) {
+  try {
+    const { product, price, thumbnail } = req.body;
+    if (product && price && thumbnail) {
+      const newProduct = new Product({ product, price, thumbnail });
+      await newProduct.save();
+      res.redirect('/');
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something goes wrong' });
+  }
+}
+
+export async function getProductById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const item = await Product.findById({ id });
+    item
+      ? res.status(200).json()
+      : res.status(404).json({ error: 'item dont found' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something goes wrong' });
+  }
+}
+
+export async function editProduct(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const { product, price, thumbnail } = req.body;
+    await Product.findOneAndUpdate(
+      { id },
+      {
+        product,
+        price,
+        thumbnail,
+      }
+    );
+    res.status(200).json({ message: 'Edited with success' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something goes wrong' });
+  }
+}
+
+export async function deleteProduct(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete({ id });
+    res.status(200).json({ message: 'Object removed successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something goes wrong' });
+  }
+}
